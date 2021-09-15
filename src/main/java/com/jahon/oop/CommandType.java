2cad0381fe8d8bc58247a7bdb0de74a7cde76620
isabaevjahongir@gmail.com <ASDADdasd12#@!#!@31dsasad>
2021-09-15 18:58:35 +0300

    Initial commit

---
+++ src/main/java/com/jahon/oop/CommandType.java
@@ -0,0 +1,5 @@
+package com.jahon.oop;
+
+public enum CommandType {
+    LIGHT_OFF
+}
---
+++ src/main/java/com/jahon/oop/SensorEventType.java
@@ -0,0 +1,5 @@
+package com.jahon.oop;
+
+public enum SensorEventType {
+    LIGHT_ON, LIGHT_OFF, DOOR_OPEN, DOOR_CLOSED
+}
